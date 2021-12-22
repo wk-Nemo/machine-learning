b5dbb178ba33834793b6de80e0a131efca1e7f96
wk-Nemo <2181544173@qq.com>
2021-12-22 17:56:04 +0800

    feat: GD and SGD

--- boston-house/train/DGTrain.py
+++ boston-house/train/DGTrain.py
@@ -6,7 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D
 ## 处理数据
 def load_data():
     # 从文件导入数据
-    datafile = './housing.data'
+    datafile = './data/housing.data'
     data = np.fromfile(datafile, sep=' ')
 
     # 每条数据包括14项，其中前面13项是影响因素，第14项是相应的房屋价格中位数
@@ -37,93 +37,72 @@ def load_data():
     training_data = data[:offset]
     test_data = data[offset:]
     return training_data, test_data
-  
-# 获取数据
-training_data, test_data = load_data()
-x = training_data[:, :-1]
-y = training_data[:, -1:]
-
-# 查看数据
-# print(x[0])
-# print(y[0])
-
-## 模型设计
-# 权重
-# w = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, -0.1, -0.2, -0.3, -0.4, 0.0]
-# w = np.array(w).reshape([13, 1])
-
-# # 取出第1条样本数据，观察样本的特征向量与参数向量相乘的结果。
-# x1 = x[0]
-# t = np.dot(x1, w)
-# print(t)
-
-# # 取b值
-# b = -0.2
-# z = t + b
-# print(z)
 
 class Network(object):
-  def __init__(self, num_of_weights):
-     # 随机产生w的初始值
-        # 为了保持程序每次运行结果的一致性，
-        # 此处设置固定的随机数种子
+    def __init__(self, num_of_weights):
+        # 随机产生w的初始值
+        # 为了保持程序每次运行结果的一致性，此处设置固定的随机数种子
         np.random.seed(0)
         self.w = np.random.randn(num_of_weights, 1)
         self.b = 0.
-  
-  def forward(self, x):
+        
+    def forward(self, x):
         z = np.dot(x, self.w) + self.b
         return z
-     
-  ## 损失函数 
-  def loss(self, z, y):
+    
+    def loss(self, z, y):
         error = z - y
+        num_samples = error.shape[0]
         cost = error * error
-        cost = np.mean(cost)
+        cost = np.sum(cost) / num_samples
         return cost
-
-  def gradient(self, x, y):
+    
+    def gradient(self, x, y):
         z = self.forward(x)
         gradient_w = (z-y)*x
         gradient_w = np.mean(gradient_w, axis=0)
         gradient_w = gradient_w[:, np.newaxis]
         gradient_b = (z - y)
-        gradient_b = np.mean(gradient_b)
-        
+        gradient_b = np.mean(gradient_b)        
         return gradient_w, gradient_b
-   
-  def update(self, gradient_w5, gradient_w9, eta=0.01):
-        net.w[5] = net.w[5] - eta * gradient_w5
-        net.w[9] = net.w[9] - eta * gradient_w9
-        
-  def train(self, x, y, iterations=100, eta=0.01):
-      points = []
-      losses = []
-      for i in range(iterations):
-          points.append([net.w[5][0], net.w[9][0]])
-          z = self.forward(x)
-          L = self.loss(z, y)
-          gradient_w, gradient_b = self.gradient(x, y)
-          gradient_w5 = gradient_w[5][0]
-          gradient_w9 = gradient_w[9][0]
-          self.update(gradient_w5, gradient_w9, eta)
-          losses.append(L)
-          if i % 50 == 0:
-              print('iter {}, point {}, loss {}'.format(i, [net.w[5][0], net.w[9][0]], L))
-      return points, losses   
     
+    def update(self, gradient_w, gradient_b, eta = 0.01):
+        self.w = self.w - eta * gradient_w
+        self.b = self.b - eta * gradient_b
+        
+    def train(self, x, y, iterations=100, eta=0.01):
+        losses = []
+        for i in range(iterations):
+            z = self.forward(x)
+            L = self.loss(z, y)
+            gradient_w, gradient_b = self.gradient(x, y)
+            self.update(gradient_w, gradient_b, eta)
+            losses.append(L)
+            if (i+1) % 10 == 0:
+                print('iter {}, loss {}'.format(i, L))
+        return losses
+
 # 获取数据
 train_data, test_data = load_data()
 x = train_data[:, :-1]
 y = train_data[:, -1:]
 # 创建网络
 net = Network(13)
-num_iterations=2000
+num_iterations = 1000
 # 启动训练
-points, losses = net.train(x, y, iterations=num_iterations, eta=0.01)
+losses = net.train(x, y, iterations=num_iterations, eta=0.01)
 
-# 画出损失函数的变化趋势
-plot_x = np.arange(num_iterations)
-plot_y = np.array(losses)
-plt.plot(plot_x, plot_y)
-plt.show()
+w = net.w
+b = net.b
+
+print('------------------------')
+print(w)
+print(b)
+print('------------------------')
+
+
+# # 画出损失函数的变化趋势
+# plot_x = np.arange(num_iterations)
+# plot_y = np.array(losses)
+# plt.plot(plot_x, plot_y)
+# plt.show()
